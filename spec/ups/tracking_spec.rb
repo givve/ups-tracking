1a1d015c8c163e69531c9c95db4d0410bf136381
Leo Arnold <larnold@givve.com>
2018-12-11 18:25:58 +0100

    Fix spec tests

--- spec/ups/tracking_spec.rb
+++ spec/ups/tracking_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-describe UPS::Tracking do
+RSpec.describe UPS::Tracking do
   it "has a semantic version number" do
     expect(UPS::Tracking::VERSION).to match(/\A\d+\.\d+\.\d+/)
   end
