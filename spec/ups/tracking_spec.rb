e80cb52e17a6d6cd88ab09fb47366e66146f3bb4
Leo Arnold <larnold@givve.com>
2020-08-14 03:32:23 +0200

    Configure development environment

--- spec/ups/tracking_spec.rb
+++ spec/ups/tracking_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe Ups::Tracking do
-  it "has a version number" do
-    expect(Ups::Tracking::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has a semantic version number" do
+    expect(Ups::Tracking::VERSION).to match /\A\d+\.\d+\.\d+/
   end
 end
